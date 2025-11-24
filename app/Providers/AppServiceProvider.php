e99ed02a248bc0cedb976c97bf5d1aa6855e285b
Jarvyes14 <javierbarcelosantos@gmail.com>
2025-11-24 09:10:15 -0600

    fix: Railway proxy modify petitions as https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Siempre que estemos detrás de un proxy confiable
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');   // ← generará https:// para assets
+            Request::setTrustedProxies(
+                ['127.0.0.1',       // localhost
+                    $this->app['request']->server->get('REMOTE_ADDR')], // Railway balancer
+                Request::HEADER_X_FORWARDED_AWS_ELB   // compatible con Railway
+            );
+        }
     }
 }
